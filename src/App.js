18c1ce838f53d87ccc78b5c3548cdc1300dfa29d
Sandipbgn <iamsbgn@gmail.com>
2022-07-28 17:43:31 +0545

    Logic added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,84 @@
 import React from "react";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/', '*', '+', '-', '.'];
+
+  const updatecalc = value => {
+    if (
+      ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1)
+      )
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+
+  }
+
+  function equals() {
+    setCalc(eval(calc).toString());
+
+  }
+
+  function deletelast() {
+    if (calc == '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  }
+  const c = () =>{
+    window.location.reload();
+  }
+
   return (
     <div className="app">
       <div className="calc">
         <div className="display">
-          <span>(0)</span> 0
+          {result ? <span>({result})</span> : ''} {calc || "0"}
         </div>
-<div className="body">
+        <div className="body">
 
-  <button className="operator" >C</button>
- <button className="operator" >%</button>
- <button className="operator" >/</button>
- <button className="operator" >Del</button>
-
-  
+          <button className="operator"  onClick={c}>C</button>
+          <button className="operator" onClick={() => updatecalc('%')} >%</button>
+          <button className="operator" onClick={() => updatecalc('/')}>/</button>
+          <button className="operator" onClick={deletelast}>Del</button>
 
 
 
-  <button>7</button>
-  <button>8</button>
-  <button>9</button>
-  <button className="operator">x</button>
-  <button>4</button>
-  <button>5</button>
-  <button>6</button>
-  <button className="operator">-</button>
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button className="operator">/</button>
-  <button>0</button>
-  <button>.</button>
-  <button className="equals">=</button>
+
+
+          <button onClick={() => updatecalc('7')}>7</button>
+          <button onClick={() => updatecalc('8')}>8</button>
+          <button onClick={() => updatecalc('9')}>9</button>
+          <button className="operator" onClick={() => updatecalc('*')}>x</button>
+          <button onClick={() => updatecalc('4')}>4</button>
+          <button onClick={() => updatecalc('5')}>5</button>
+          <button onClick={() => updatecalc('6')}>6</button>
+          <button className="operator" onClick={() => updatecalc('-')}>-</button>
+          <button onClick={() => updatecalc('1')}>1</button>
+          <button onClick={() => updatecalc('2')}>2</button>
+          <button onClick={() => updatecalc('3')}>3</button>
+          <button className="operator" onClick={() => updatecalc('/')}>/</button>
+          <button onClick={() => updatecalc('0')}>0</button>
+          <button onClick={() => updatecalc('.')}>.</button>
+          <button className="equals" onClick={equals} >=</button>
 
 
 
-</div>
+        </div >
 
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
